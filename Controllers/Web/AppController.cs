3f65407b723081b0ae247fc1e813cab6a92a00e0
Anthony Balletta <anthonyballetta@gmail.com>
2017-10-08 15:53:03 -0400

    Updates

--- Controllers/Web/AppController.cs
+++ Controllers/Web/AppController.cs
@@ -5,23 +5,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TheWorld.ViewModels;
+using TheWorld.Services;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        //public IActionResult Contact()
-        //{
-        //    return View();
-        //}
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [HttpPost]
         public IActionResult Contact(ContactViewModel model)
         {
+            _mailService.SendMail("anthonyballetta@gmail.com", model.Email, "From TheWorld", model.Message);
             return View();
         }
 
